93043b2e89f86e5272608f834263402f860166f7
Joachim Jablon <ewjoachim@gmail.com>
2022-12-01 22:57:36 +0100

    Update boilerplate.py

--- boilerplate.py
+++ boilerplate.py
@@ -1,6 +1,4 @@
 import requests, browsercookie, pathlib
 
 def get_input_lines(year, day):
-    text = requests.get(f"https://adventofcode.com/{year}/day/{day}/input", cookies=browsercookie.firefox()).text
-    pathlib.Path("i").write_text(text)
-    return text
+    return requests.get(f"https://adventofcode.com/{year}/day/{day}/input", cookies=browsercookie.firefox()).text
